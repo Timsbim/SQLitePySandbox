4a4c0f75f013c0f5f112cbc5b2498915f1e07e62
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-04-16 20:34:17 +0000

    Update

--- current.py
+++ current.py
@@ -30,6 +30,81 @@ SQL_PATH.mkdir(parents=True, exist_ok=True)
 # Exercism SQLite path exercises
 
 
+def high_scores():
+    """Exercism SQLite path exercise 18, High Scores:
+    https://exercism.org/tracks/sqlite/exercises/high-scores"""
+
+    sql_path = SQL_PATH / "High-Scores"
+    sql_path.mkdir(parents=True, exist_ok=True)
+    
+    with sqlite.connect(":memory:") as con:
+        query = dedent("""\
+            CREATE TABLE scores (game_id TEXT, score INT);
+            CREATE TABLE results (game_id TEXT, property TEXT, result TEXT);
+            INSERT INTO results(game_id, property)
+                VALUES
+                    ('1035eb93-2208-4c22-bab8-fef06769a73c', 'scores'),
+                    ('6aa5dbf5-78fa-4375-b22c-ffaa989732d2', 'latest'),
+                    ('b661a2e1-aebf-4f50-9139-0fb817dd12c6', 'personalBest'),
+                    ('3d996a97-c81c-4642-9afc-80b80dc14015', 'personalTopThree'),
+                    ('1084ecb5-3eb4-46fe-a816-e40331a4e83a', 'personalTopThree'),
+                    ('e6465b6b-5a11-4936-bfe3-35241c4f4f16', 'personalTopThree'),
+                    ('f73b02af-c8fd-41c9-91b9-c86eaa86bce2', 'personalTopThree'),
+                    ('16608eae-f60f-4a88-800e-aabce5df2865', 'personalTopThree'),
+                    ('2df075f9-fec9-4756-8f40-98c52a11504f', 'latest'),
+                    ('809c4058-7eb1-4206-b01e-79238b9b71bc', 'scores'),
+                    ('ddb0efc0-9a86-4f82-bc30-21ae0bdc6418', 'latest'),
+                    ('6a0fd2d1-4cc4-46b9-a5bb-2fb667ca2364', 'scores');
+            """)
+        con.executescript(query)
+        inputs = (
+            ('1035eb93-2208-4c22-bab8-fef06769a73c', '30,50,20,70'),
+            ('6aa5dbf5-78fa-4375-b22c-ffaa989732d2', '100,0,90,30'),
+            ('b661a2e1-aebf-4f50-9139-0fb817dd12c6', '40,100,70'),
+            ('3d996a97-c81c-4642-9afc-80b80dc14015', '10,30,90,30,100,20,10,0,30,40,40,70,70'),
+            ('1084ecb5-3eb4-46fe-a816-e40331a4e83a', '20,10,30'),
+            ('e6465b6b-5a11-4936-bfe3-35241c4f4f16', '40,20,40,30'),
+            ('f73b02af-c8fd-41c9-91b9-c86eaa86bce2', '30,70'),
+            ('16608eae-f60f-4a88-800e-aabce5df2865', '40'),
+            ('2df075f9-fec9-4756-8f40-98c52a11504f', '70,50,20,30'),
+            ('809c4058-7eb1-4206-b01e-79238b9b71bc', '30,50,20,70'),
+            ('ddb0efc0-9a86-4f82-bc30-21ae0bdc6418', '20,70,15,25,30'),
+            ('6a0fd2d1-4cc4-46b9-a5bb-2fb667ca2364', '20,70,15,25,30')
+        )
+        data = (
+            (ID, int(score))
+            for ID, string in inputs
+            for score in string.split(",")
+        )
+        con.executemany("INSERT INTO scores VALUES(?, ?)", data)
+        query = dedent("""\
+            SELECT game_id,
+                   group_concat(score, ',') scores,
+                   max(score) personalBest
+            FROM scores
+            GROUP BY game_id
+            """)
+        query = dedent("""\
+            SELECT results.game_id,
+                   (SELECT group_concat(score, '-')
+                    FROM scores
+                    WHERE scores.game_id = results.game_id
+                    ORDER BY score ASC
+                    LIMIT 3)
+            FROM results, scores
+            WHERE results.game_id = scores.game_id
+            GROUP BY results.game_id
+            """)
+        #print_query(query, filepath=sql_path / "solution.sql")        
+        #con.execute(query)
+        #query = "SELECT * FROM results;"
+        res = con.execute(query)
+        pprint(res.fetchall())
+
+
+high_scores()
+
+
 def luhn():
     """Exercism SQLite path exercise 20, Luhn:
     https://exercism.org/tracks/sqlite/exercises/luhn"""
