5724d6b0928ee5c16dac4d53d3cc38a56cdbb78c
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-04-21 11:22:50 +0000

    Update

--- current.py
+++ current.py
@@ -53,7 +53,7 @@ def yacht():
                 (row[:2] for row in csv.reader(file))
             )
         query = dedent("""\
-            WITH RECURSIVE rolls(res, cat, p, roll) AS (
+            WITH rolls(res, cat, p, roll) AS (
                 SELECT dice_results, category, 4, CAST(substr(dice_results, 1, 1) AS INT) FROM yacht
                 UNION ALL
                 SELECT res, cat, p + 3, CAST(substr(res, p, 1) AS INT) FROM rolls WHERE p < 14
@@ -63,15 +63,16 @@ def yacht():
             ),
             results(res, cat, val) AS (
                 SELECT
-                    res, cat,
+                    res,
+                    cat,
                     CASE cat
-                        WHEN 'ones' THEN ifnull(sum(score) FILTER (WHERE roll IS 1), 0)
-                        WHEN 'twos' THEN ifnull(sum(score) FILTER (WHERE roll IS 2), 0)
-                        WHEN 'threes' THEN ifnull(sum(score) FILTER (WHERE roll IS 3), 0)
-                        WHEN 'fours' THEN ifnull(sum(score) FILTER (WHERE roll IS 4), 0)
-                        WHEN 'fives' THEN ifnull(sum(score) FILTER (WHERE roll IS 5), 0)
-                        WHEN 'sixes' THEN ifnull(sum(score) FILTER (WHERE roll IS 6), 0)
-                        WHEN 'full house' THEN iif(group_concat(num, '') in ('23', '32'), sum(score), 0)
+                        WHEN 'ones' THEN ifnull(sum(score) FILTER (WHERE roll = 1), 0)
+                        WHEN 'twos' THEN ifnull(sum(score) FILTER (WHERE roll = 2), 0)
+                        WHEN 'threes' THEN ifnull(sum(score) FILTER (WHERE roll = 3), 0)
+                        WHEN 'fours' THEN ifnull(sum(score) FILTER (WHERE roll = 4), 0)
+                        WHEN 'fives' THEN ifnull(sum(score) FILTER (WHERE roll = 5), 0)
+                        WHEN 'sixes' THEN ifnull(sum(score) FILTER (WHERE roll = 6), 0)
+                        WHEN 'full house' THEN iif(group_concat(num, '') IN ('23', '32'), sum(score), 0)
                         WHEN 'four of a kind' THEN CASE num
                                 WHEN 5 THEN score - roll
                                 ELSE ifnull(sum(score) FILTER (WHERE num = 4), 0)
@@ -79,8 +80,8 @@ def yacht():
                         WHEN 'little straight' THEN iif(group_concat(roll, '') = '12345', 30, 0)
                         WHEN 'big straight' THEN iif(group_concat(roll, '') = '23456', 30, 0)
                         WHEN 'choice' THEN sum(score)
-                        WHEN 'yacht' THEN iif(sum(score) FILTER (WHERE num IS 5) IS NULL, 0, 50)
-                    END
+                        WHEN 'yacht' THEN iif(num = 5, 50, 0)
+                   END
                 FROM groups
                 GROUP BY res, cat
                 ORDER BY roll
--- sql/Exercism/Yacht/solution.sql
+++ sql/Exercism/Yacht/solution.sql
@@ -1,4 +1,4 @@
-WITH RECURSIVE rolls(res, cat, p, roll) AS (
+WITH rolls(res, cat, p, roll) AS (
     SELECT dice_results, category, 4, CAST(substr(dice_results, 1, 1) AS INT) FROM yacht
     UNION ALL
     SELECT res, cat, p + 3, CAST(substr(res, p, 1) AS INT) FROM rolls WHERE p < 14
@@ -8,15 +8,16 @@ groups(res, cat, roll, num, score) AS (
 ),
 results(res, cat, val) AS (
     SELECT
-        res, cat,
+        res,
+        cat,
         CASE cat
-            WHEN 'ones' THEN ifnull(sum(score) FILTER (WHERE roll IS 1), 0)
-            WHEN 'twos' THEN ifnull(sum(score) FILTER (WHERE roll IS 2), 0)
-            WHEN 'threes' THEN ifnull(sum(score) FILTER (WHERE roll IS 3), 0)
-            WHEN 'fours' THEN ifnull(sum(score) FILTER (WHERE roll IS 4), 0)
-            WHEN 'fives' THEN ifnull(sum(score) FILTER (WHERE roll IS 5), 0)
-            WHEN 'sixes' THEN ifnull(sum(score) FILTER (WHERE roll IS 6), 0)
-            WHEN 'full house' THEN iif(group_concat(num, '') in ('23', '32'), sum(score), 0)
+            WHEN 'ones' THEN ifnull(sum(score) FILTER (WHERE roll = 1), 0)
+            WHEN 'twos' THEN ifnull(sum(score) FILTER (WHERE roll = 2), 0)
+            WHEN 'threes' THEN ifnull(sum(score) FILTER (WHERE roll = 3), 0)
+            WHEN 'fours' THEN ifnull(sum(score) FILTER (WHERE roll = 4), 0)
+            WHEN 'fives' THEN ifnull(sum(score) FILTER (WHERE roll = 5), 0)
+            WHEN 'sixes' THEN ifnull(sum(score) FILTER (WHERE roll = 6), 0)
+            WHEN 'full house' THEN iif(group_concat(num, '') IN ('23', '32'), sum(score), 0)
             WHEN 'four of a kind' THEN CASE num
                     WHEN 5 THEN score - roll
                     ELSE ifnull(sum(score) FILTER (WHERE num = 4), 0)
@@ -24,8 +25,8 @@ results(res, cat, val) AS (
             WHEN 'little straight' THEN iif(group_concat(roll, '') = '12345', 30, 0)
             WHEN 'big straight' THEN iif(group_concat(roll, '') = '23456', 30, 0)
             WHEN 'choice' THEN sum(score)
-            WHEN 'yacht' THEN iif(sum(score) FILTER (WHERE num IS 5) IS NULL, 0, 50)
-        END
+            WHEN 'yacht' THEN iif(num = 5, 50, 0)
+       END
     FROM groups
     GROUP BY res, cat
     ORDER BY roll
