a68ad2b358dca52cdc05acb7071ff9ae529085b4
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-04-22 19:59:11 +0000

    Update

--- current.py
+++ current.py
@@ -61,12 +61,30 @@ def meetup():
                 (row[:4] for row in csv.reader(file))
             )
         query = dedent("""\
-            UPDATE meetup
-            SET result =
+            WITH daysofweek(dayofweek, day_no) AS (
+                VALUES ('Monday', 0), ('Tuesday', 1), ('Wednesday', 2),
+                       ('Thursday', 3), ('Friday', 4), ('Saturday', 5),
+                       ('Sunday', 6)
+            ),
+            weeks(week, week_no) AS (
+                VALUES ('first', 0), ('second', 1), ('third', 2), ('fourth', 3)
+            )
+            SELECT year, month, week,
+                   day_no,
+                   CASE week
+                       WHEN 'teenth' THEN format('%i-%.2i-%.2i', year, month, 10)
+                       WHEN 'last' THEN
+                           date(format('%i-%.2i-%.2i', year, month, 1), '1 months', '-1 days')
+                       ELSE format('%i-%.2i-%.2i', year, month, week_no * 7 + 1)
+                   END
+            FROM meetup
+                 LEFT JOIN daysofweek USING(dayofweek)
+                 LEFT JOIN weeks USING(week)
+            ;
             """)
-        #print_query(query, filepath=sql_path / "solution.txt")
+        #print_query(query, filepath=sql_path / "solution.sql")
         #con.execute(query)
-        query = "SELECT * FROM meetup;"
+        #query = "SELECT * FROM meetup;"
         res = con.execute(query)
         pprint(res.fetchall())
  
