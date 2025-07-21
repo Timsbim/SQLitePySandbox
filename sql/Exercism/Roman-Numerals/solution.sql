56baa748cef380ca60d440f47ba1689cedfcfdf6
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2025-07-21 18:27:28 +0000

    Update

--- sql/Exercism/Roman-Numerals/solution.sql
+++ sql/Exercism/Roman-Numerals/solution.sql
@@ -6,22 +6,22 @@ rms(n, i, d, r, p, rm) AS (
   	FROM "roman-numerals"
   	UNION
   	SELECT n, i + 2, r / p, r % p, p / 10,
-  		   CASE
-  		       WHEN d = 0 THEN ''
-  			   WHEN d < 4 THEN
-  			       printf('%.*c', d, (SELECT n FROM ns WHERE ns.i = rms.i))
-  			   WHEN d = 4 THEN
-  				   (SELECT n FROM ns WHERE ns.i = rms.i)
-  				     || (SELECT n FROM ns WHERE ns.i = rms.i - 1)
-  			   WHEN d = 5 THEN
-  				   (SELECT n FROM ns WHERE ns.i = rms.i - 1)
-  			   WHEN d < 9 THEN
-  				   (SELECT n FROM ns WHERE ns.i = rms.i - 1)
-  				     || printf('%.*c', d-5, (SELECT n FROM ns WHERE ns.i = rms.i))
-  			   ELSE
-  			       (SELECT n FROM ns WHERE ns.i = rms.i)
-  				     || (SELECT n FROM ns WHERE ns.i = rms.i - 2)
-  		   END
+        CASE
+            WHEN d = 0 THEN ''
+            WHEN d < 4 THEN
+                printf('%.*c', d, (SELECT n FROM ns WHERE ns.i = rms.i))
+            WHEN d = 4 THEN
+                (SELECT n FROM ns WHERE ns.i = rms.i)
+                  || (SELECT n FROM ns WHERE ns.i = rms.i - 1)
+            WHEN d = 5 THEN
+                (SELECT n FROM ns WHERE ns.i = rms.i - 1)
+            WHEN d < 9 THEN
+                (SELECT n FROM ns WHERE ns.i = rms.i - 1)
+                  || printf('%.*c', d-5, (SELECT n FROM ns WHERE ns.i = rms.i))
+            ELSE
+                (SELECT n FROM ns WHERE ns.i = rms.i)
+                  || (SELECT n FROM ns WHERE ns.i = rms.i - 2)
+        END
   	FROM rms WHERE i < 8
 )
 UPDATE "roman-numerals"
