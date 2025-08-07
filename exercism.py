c1ab95ffab2cd6c9916c61a112244366e60638d7
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2025-08-07 19:50:00 +0000

    Update

--- exercism.py
+++ exercism.py
@@ -57,7 +57,36 @@ def anagram():
                 """INSERT INTO anagram(subject, candidates) VALUES(?, json(?));""",
                 csv.reader(file, delimiter=";")
             )
-       
+
+        stmt = dedent("""\
+            UPDATE anagram
+            SET result = (
+              	WITH tmp1(low, i, c) AS (
+                	SELECT lower(subject), 2, substr(lower(subject), 1, 1)
+                    UNION
+                    SELECT low, i+1, substr(low, i, 1) FROM tmp1 WHERE i <= length(low)
+                ),
+                word(low, norm) AS (
+                	SELECT low, group_concat(c, '')
+                  	FROM (SELECT * FROM tmp1 ORDER BY c) GROUP BY low
+                ),
+              	tmp2(id, word, low, i, c) AS (
+                	SELECT id, value, lower(value), 2, substr(lower(value), 1, 1) FROM json_each(candidates)
+                    UNION
+                  	SELECT id, word, low, i+1, substr(low, i, 1) FROM tmp2 WHERE i <= length(word)
+                ),
+              	words(word, low, norm) AS (
+                	SELECT word, low, group_concat(c, '')
+                    FROM (SELECT * FROM tmp2 ORDER BY id, c) GROUP BY id
+                )
+            	SELECT json_group_array(word)
+              	FROM words, word
+                WHERE words.low != word.low AND words.norm = word.norm
+            )
+            """)
+        print_stmt(stmt, filepath=sql_path / "solution.sql")
+        con.execute(stmt)
+        
         res = con.execute("""SELECT * FROM anagram;""")
         pprint(res.fetchall())
 
